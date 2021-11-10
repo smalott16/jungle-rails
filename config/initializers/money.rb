7ec54295137c6bb90072b69744cf9fedfbf63336
Spencer Malott <spencermalott16@gmail.com>
2021-11-10 21:41:33 +0000

    added the sold out badge to products with quantity of zero. Had to update the seeds file and reset the db in order to see if conditionl code was working

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -78,7 +78,7 @@ MoneyRails.configure do |config|
   config.no_cents_if_whole = false
   # config.default_format = {
   #   no_cents_if_whole: false,
-    # symbol: nil,
-    # sign_before_symbol: nil
-  #}
+  #   # symbol: nil,
+  #   # sign_before_symbol: nil
+  # }
 end
