d836e59ecafeacdd1246711ff83a42452609f61b
Spencer Malott <spencermalott16@gmail.com>
2021-11-11 20:54:44 +0000

    Generated a new user model and ran the db migration. Created routes for signup and create user. Generated a controller and started on a sign up page.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,7 @@
 class Admin::CategoriesController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USER'], password: ENV['HTTP_BASIC_AUTH_PWD']
+  
   def index
     @categories = Category.order(id: :desc).all
   end
