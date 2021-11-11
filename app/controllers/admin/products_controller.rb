4301e51d11c0e3f6fc451b564b020c6033daa591
Spencer Malott <spencermalott16@gmail.com>
2021-11-11 00:59:05 +0000

    basic http auth using environment variables. The change is mainly noticeable when using an incognito window. Users now have to enter a username and pwd to access the admin products page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USER'], password: ENV['HTTP_BASIC_AUTH_PWD']
 
   def index
     @products = Product.order(id: :desc).all
