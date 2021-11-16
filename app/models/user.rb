d595e21094899e27e55eed6f93ef7f6c1cffdc1a
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-11-16 04:25:32 +0000

    added the authenticate wit crediential class method to the user class.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,23 @@
 class User < ActiveRecord::Base
   
   has_secure_password
-  
+
   validates :name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   validates :password, presence: true
-  validates :password_confirmation, presence: true
   validates :password, length: { minimum: 4 }
+  validates :password_confirmation, presence: true
+  
+  private
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email)
+    if @user && @user.authenticate(password)
+      @user
+    else 
+      nil
+    end
+  end
+
 end
